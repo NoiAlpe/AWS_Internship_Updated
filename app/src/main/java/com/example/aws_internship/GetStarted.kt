c59032e8c6b8048063f89ae9b216c5d1df71849e
NoiAlpe <honesto.alpe@awsys-i.com>
2023-01-03 16:23:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aws_internship/GetStarted.kt
@@ -0,0 +1,15 @@
+package com.example.aws_internship
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GetStarted : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+
+        supportActionBar?.hide()
+
+
+    }
+}
