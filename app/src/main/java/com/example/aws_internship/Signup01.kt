1fd00750b2f07a467e5e046c2b8cb8b9ae14b466
robert-flores-aws <robert.flores@awsys-i.com>
2023-01-04 16:23:51 +0800

    FInished

---
+++ app/src/main/java/com/example/aws_internship/Signup01.kt
@@ -0,0 +1,13 @@
+package com.example.aws_internship
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup01 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_01)
+
+
+    }
+}
