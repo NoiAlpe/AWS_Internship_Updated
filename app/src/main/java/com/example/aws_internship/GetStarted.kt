6f874117c17466f36921a61b5aeddba4e9a765dd
robert-flores-aws <robert.flores@awsys-i.com>
2023-01-04 16:23:44 +0800

    FInished

--- app/src/main/java/com/example/aws_internship/GetStarted.kt
+++ app/src/main/java/com/example/aws_internship/GetStarted.kt
@@ -1,15 +1,28 @@
 package com.example.aws_internship
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
 
-class GetStarted : AppCompatActivity() {
+class GetStarted : AppCompatActivity(){
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_started)
 
-        supportActionBar?.hide()
 
+        val btnGetStarted = findViewById<Button>(R.id.btnGetStarted)
+
+        btnGetStarted.setOnClickListener {
+            val intent = Intent(this@GetStarted, Signup01::class.java)
+            startActivity(intent)
+        }
 
     }
+
+
 }
--- app/src/main/java/com/example/aws_internship/MainActivity.kt
+++ app/src/main/java/com/example/aws_internship/MainActivity.kt
@@ -17,5 +17,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }, 1500)
+
     }
 }
