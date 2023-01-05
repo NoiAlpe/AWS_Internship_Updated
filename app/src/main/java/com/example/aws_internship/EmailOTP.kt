b2d4c1ecefd952e463e1ddb4c566116c151548d9
NoiAlpe <honesto.alpe@awsys-i.com>
2023-01-05 15:59:26 +0800

    Verify Email done,

--- app/src/main/java/com/example/aws_internship/EmailOTP.kt
+++ app/src/main/java/com/example/aws_internship/EmailOTP.kt
@@ -1,11 +1,27 @@
 package com.example.aws_internship
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aws_internship.databinding.ActivityEmailOtpBinding
+import com.example.aws_internship.databinding.ActivitySignUpBinding
 
 class EmailOTP : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEmailOtpBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_email_otp)
+
+        binding = ActivityEmailOtpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnProceed.setOnClickListener {
+            val intent = Intent(this@EmailOTP, ProfileDetails::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/aws_internship/SignUp.kt
+++ app/src/main/java/com/example/aws_internship/SignUp.kt
@@ -1,11 +1,27 @@
 package com.example.aws_internship
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aws_internship.databinding.ActivitySignUpBinding
+import com.example.aws_internship.databinding.ActivitySignup01Binding
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnProceed.setOnClickListener {
+                val intent = Intent(this@SignUp, EmailOTP::class.java)
+                startActivity(intent)
+        }
+
     }
 }
