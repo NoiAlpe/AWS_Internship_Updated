8083d4906f3ea311476f9976725206f0c91a3cb0
robert-flores-aws <robert.flores@awsys-i.com>
2023-01-05 16:28:14 +0800

    Update Login Credentials, Forgot Password 01 and 02

--- app/src/main/java/com/example/aws_internship/LoginCredentials.kt
+++ app/src/main/java/com/example/aws_internship/LoginCredentials.kt
@@ -2,10 +2,15 @@ package com.example.aws_internship
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aws_internship.databinding.ActivityEmailOtpBinding
 
 class LoginCredentials : AppCompatActivity() {
+
+    private lateinit var binding: LoginCredentials
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_credentials)
+
     }
 }
