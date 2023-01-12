2bb80f8ae0df996a9b6b8f074b2f18b778090510
robert-flores-aws <robert.flores@awsys-i.com>
2023-01-12 16:09:43 +0800

    Update LoginCredentials, ForgotPassword01. Added new page Homepage

--- app/src/main/java/com/example/aws_internship/ForgotPassword01.kt
+++ app/src/main/java/com/example/aws_internship/ForgotPassword01.kt
@@ -3,11 +3,16 @@ package com.example.aws_internship
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import androidx.core.view.isVisible
 import com.example.aws_internship.databinding.ActivityForgotPassword01Binding
 
 class ForgotPassword01 : AppCompatActivity() {
 
     private lateinit var binding: ActivityForgotPassword01Binding
+    private var temporaryOTP: String = "012345"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +21,145 @@ class ForgotPassword01 : AppCompatActivity() {
         binding = ActivityForgotPassword01Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnProceed.setOnClickListener{
-            val intent = Intent(this@ForgotPassword01, ForgotPassword02::class.java)
+        binding.btnProceed.setOnClickListener {
+
+            if (temporaryOTP == (binding.tiOPP0.text.toString()
+                        + binding.tiOPP1.text.toString()
+                        + binding.tiOPP2.text.toString()
+                        + binding.tiOPP3.text.toString()
+                        + binding.tiOPP4.text.toString()
+                        + binding.tiOPP5.text.toString())
+            ) {
+                Toast.makeText(this@ForgotPassword01,"OTP Accepted", Toast.LENGTH_SHORT).show()
+
+                val sharedPreferences = getSharedPreferences(MainActivity().preferenceName, 0)
+                var editor = sharedPreferences.edit()
+
+                editor.putBoolean("hasLoggedIn",true)
+                editor.commit()
+
+
+                val intent = Intent(this@ForgotPassword01, ForgotPassword02::class.java)
+                binding.ivHelperText.isVisible = false
+                startActivity(intent)
+                finish()
+            } else {
+                binding.ivHelperText.isVisible = true
+            }
+
+        }
+
+        binding.tiOPP0.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (binding.tiOPP0.text.toString().length == 1) {
+                    binding.tiOPP1.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+
+        binding.tiOPP1.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (binding.tiOPP1.text.toString().length == 1) {
+                    binding.tiOPP2.requestFocus()
+                } else {
+                    binding.tiOPP0.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+
+        binding.tiOPP2.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (binding.tiOPP2.text.toString().length == 1) {
+                    binding.tiOPP3.requestFocus()
+                } else {
+                    binding.tiOPP1.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+        binding.tiOPP3.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (binding.tiOPP3.text.toString().length == 1) {
+                    binding.tiOPP4.requestFocus()
+                } else {
+                    binding.tiOPP2.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+        binding.tiOPP4.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (binding.tiOPP4.text.toString().length == 1) {
+                    binding.tiOPP5.requestFocus()
+                } else {
+                    binding.tiOPP3.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+
+        binding.tiOPP5.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (binding.tiOPP5.text.toString().isEmpty()) {
+                    binding.tiOPP4.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+
+        binding.ibBackButton.setOnClickListener {
+            val intent = Intent (this@ForgotPassword01, LoginCredentials::class.java)
             startActivity(intent)
-            finish()
         }
     }
 }
--- app/src/main/java/com/example/aws_internship/LoginCredentials.kt
+++ app/src/main/java/com/example/aws_internship/LoginCredentials.kt
@@ -3,11 +3,15 @@ package com.example.aws_internship
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.aws_internship.databinding.ActivityLoginCredentialsBinding
 
 class LoginCredentials : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginCredentialsBinding
+
+    private var samplePass = "Admin123"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +27,47 @@ class LoginCredentials : AppCompatActivity() {
         }
 
         binding.btnLoginContinue.setOnClickListener{
+            val match = passwordValidation(binding.tiPassword.text.toString())
+            if (match){
+                binding.tilPassword.helperText = null
+                binding.tilPassword.error = null
+                val intent = Intent(this@LoginCredentials, HomePage::class.java)
+                startActivity(intent)
+            } else {
+                binding.tilPassword.error = "Password does not match"
+            }
+
 
         }
+// TODO: If want to check via text changed in textview, uncomment the onTextChanged
+
+        binding.tiPassword.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                if (binding.tiPassword.text.toString().isNotEmpty()){
+                    binding.tilPassword.error = null
+                }
+
+//                val error = passwordValidation(binding.tiPassword.text.toString())
+//
+//                if (error){
+//                    binding.tilPassword.error = null
+//                    binding.tilPassword.helperText = null
+//                } else {
+//                    binding.tilPassword.error = "Password does not match"
+//                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
 
     }
+
+    private fun passwordValidation(password: String) : Boolean = password == samplePass
 }
