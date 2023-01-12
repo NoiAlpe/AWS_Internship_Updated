c1b497357201b7fab7fb7dfca39653fda4c3aea3
robert-flores-aws <robert.flores@awsys-i.com>
2023-01-12 16:50:25 +0800

    Update Avatar

--- app/src/main/java/com/example/aws_internship/AddAvatar.kt
+++ app/src/main/java/com/example/aws_internship/AddAvatar.kt
@@ -3,6 +3,7 @@ package com.example.aws_internship
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.example.aws_internship.databinding.ActivityAddAvatarBinding
 import com.example.aws_internship.databinding.ActivityEmailOtpBinding
 import com.example.aws_internship.databinding.ActivityProfileDetailsBinding
@@ -10,6 +11,10 @@ import com.example.aws_internship.databinding.ActivityProfileDetailsBinding
 class AddAvatar : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddAvatarBinding
+
+    companion object {
+        val image_request_code = 100
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +24,38 @@ class AddAvatar : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        binding.ibAddAvatar.setOnClickListener {
+            pickImageGallery()
+        }
+
         binding.ibBackButton.setOnClickListener {
             val intent = Intent(this@AddAvatar, ProfileDetails::class.java)
             startActivity(intent)
         }
 
+        binding.ibRemoveAvatar.setOnClickListener {
+            binding.imageView.setImageResource(R.drawable.aws_logo)
+            binding.ibRemoveAvatar.isVisible = false
+        }
+
+        binding.btnProceed.setOnClickListener{
+            val intent = Intent(this@AddAvatar, HomePage::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, image_request_code)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == image_request_code && resultCode == RESULT_OK){
+            binding.imageView.setImageURI(data?.data)
+            binding.ibRemoveAvatar.isVisible = true
+        }
     }
 }
